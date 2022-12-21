1c18ac6fb350901cf4cd4861933650529d1ecfe0
Dmitry <2903015@gmail.com>
2022-12-21 15:21:30 +0300

    v.1

--- pages/index.vue
+++ pages/index.vue
@@ -7,25 +7,47 @@
       <button class="btn" @click="add">Add</button>
       {{ count }}
 
+      <p>--{{ pending }}--</p>
+
     </ul>
   </div>
 </template>
 
-<script setup lang="ts">
+<script setup>
   console.log("RUN: index.vue")
 
   definePageMeta({
     layout: "default",
   })
 
+  useHead({
+    title: 'ToDo App',
+    meta: [
+      {
+        name: 'description',
+        content: 'Nuxt 3 ToDo App with Composition API'
+      }
+    ]
+  })
+
   let count = ref(0);
   const add = () => {
     count.value++
+    refresh()
   }
 
-  // const store = useStore
-  
+  const foo = useCookie("foo")
+  foo.value = 'data'
+  console.log("FOO: ", foo.value);
 
+  // const getApiData = () = {
+  //   return useFetch('/api')
+  // }
+
+  const { data: user, pending, refresh } = await useFetch('/api')
+  console.log("API: ", JSON.parse(user.value))
+
+  
 </script>
 
 <style scoped>
